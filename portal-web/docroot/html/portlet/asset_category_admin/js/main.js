bdd742ec543413079bf28af01442c203069e10bb
Shinn Lok <shinn.lok@liferay.com>
2012-10-05 14:14:09 -0700

    sourceformatting

--- portal-web/docroot/html/portlet/asset_category_admin/js/main.js
+++ portal-web/docroot/html/portlet/asset_category_admin/js/main.js
@@ -2655,7 +2655,7 @@ AUI.add(
 				EXTENDS: A.Base,
 				NAME: 'livesearch',
 				prototype: {
-					initializer: function () {
+					initializer: function() {
 						this._bindUIACBase();
 						this._syncUIACBase();
 					}
