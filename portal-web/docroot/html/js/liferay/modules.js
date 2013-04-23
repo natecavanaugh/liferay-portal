8946447113ce6701f5418acf5572a5e6f939d971
Marc Lundgren <marc.lundgren@liferay.com>
2013-04-22 19:25:04 -0700

    LPS-34665 - Upgrade alerts to bootstrap markup

--- portal-web/docroot/html/js/liferay/modules.js
+++ portal-web/docroot/html/js/liferay/modules.js
@@ -423,7 +423,7 @@ YUI.add(
 					fullpath: javaScriptRootPath + '/liferay/staging.js',
 					requires: [
 						'aui-io-plugin-deprecated',
-						'aui-modal',
+						'aui-modal'
 					]
 				},
 				'liferay-staging-branch': {
