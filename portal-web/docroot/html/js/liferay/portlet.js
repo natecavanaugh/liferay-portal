03a45c7a99a2122633edfd078fc4a6f8d88701ca
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2013-05-31 22:20:48 +0200

    LPS-36538 Fix portal/render_portlet URL generation in portlet minimizing

--- portal-web/docroot/html/js/liferay/portlet.js
+++ portal-web/docroot/html/js/liferay/portlet.js
@@ -448,16 +448,7 @@
 							after: {
 								success: function() {
 									if (restore) {
-										var data = {
-											doAsUserId: doAsUserId,
-											p_l_id: plid,
-											p_p_id: portlet.portletId,
-											p_p_state: 'exclusive'
-										};
-
-										content.plug(A.Plugin.ParseContent);
-
-										content.load(themeDisplay.getPathMain() + '/portal/render_portlet?' + A.QueryString.stringify(data));
+										content.load(options.minimizeURL);
 									}
 								}
 							},
