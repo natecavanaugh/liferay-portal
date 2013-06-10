aa829c024236facd5fc92457c85354c70cb7a885
Chema Balsas <jbalsas@gmail.com>
2013-06-10 10:47:23 +0200

    LPS-36165 Adds basic device preview support

--- portal-web/docroot/html/js/liferay/dockbar_add_page.js
+++ portal-web/docroot/html/js/liferay/dockbar_add_page.js
@@ -127,7 +127,7 @@ AUI.add(
 					_cancelAction: function(event) {
 						event.preventDefault();
 
-						Dockbar.loadPanel();
+						Dockbar._loadPanel('addPanel');
 					},
 
 					_updateActivePage: function(event) {
