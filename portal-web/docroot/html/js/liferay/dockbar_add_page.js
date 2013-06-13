148f921dd8aa788293a524a4188933430fc21985
Chema Balsas <jose.balsas@liferay.com>
2013-06-13 18:52:55 +0200

    LPS-36165 Source Formatting (as per Iliyan comments)

--- portal-web/docroot/html/js/liferay/dockbar_add_page.js
+++ portal-web/docroot/html/js/liferay/dockbar_add_page.js
@@ -142,7 +142,7 @@ AUI.add(
 					_cancelAction: function(event) {
 						event.preventDefault();
 
-						Dockbar._loadPanel('addPanel');
+						Dockbar.toggleAddPanel();
 					},
 
 					_updateActivePage: function(event) {
