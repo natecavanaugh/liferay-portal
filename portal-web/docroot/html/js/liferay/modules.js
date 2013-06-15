47d92d063d5dff318d38de4984daf0483d330fcc
Chema Balsas <jose.balsas@liferay.com>
2013-06-15 18:44:32 +0200

    LPS-36166 Animates device preview size transitions

--- portal-web/docroot/html/js/liferay/modules.js
+++ portal-web/docroot/html/js/liferay/modules.js
@@ -213,6 +213,7 @@ window.YUI_config = {
 						'aui-dialog-iframe-deprecated',
 						'aui-event-input',
 						'aui-modal',
+						'liferay-widget-size-animation-plugin',
 						'liferay-portlet-base',
 						'liferay-util',
 						'liferay-util-window'
@@ -588,6 +589,15 @@ window.YUI_config = {
 					requires: [
 						'aui-dialog-iframe-deprecated',
 						'aui-modal'
+					]
+				},
+				'liferay-widget-size-animation-plugin': {
+					path: 'widget-size-animation-plugin.js',
+					requires: [
+						'anim-base',
+						'anim-easing',
+						'plugin',
+						'widget'
 					]
 				},
 				'liferay-xml-formatter': {
