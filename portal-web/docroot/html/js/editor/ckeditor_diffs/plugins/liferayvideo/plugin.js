e34c1a07642e0e9e23f538545196205f550c8dcc
Juan G <juan.gonzalez@liferay.com>
2013-06-26 14:01:34 +0200

    LPS-37304 Modify the original plugin js files

--- portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/liferayvideo/plugin.js
+++ portal-web/docroot/html/js/editor/ckeditor_diffs/plugins/liferayvideo/plugin.js
@@ -1,31 +1,73 @@
-/*	 
-	 * Liferay Video plugin for CKEditor.
-	 * Based on Video plugin for CKEditor Copyright (C) 2011 Alfonso Martínez de
-	 * Lizarrondo
-	 *  == BEGIN LICENSE ==
-	 * 
-	 * Licensed under the terms of any of the following licenses at your choice:
-	 *  - GNU General Public License Version 2 or later (the "GPL")
-	 * http://www.gnu.org/licenses/gpl.html
-	 *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
-	 * http://www.gnu.org/licenses/lgpl.html
-	 *  - Mozilla Public License Version 1.1 or later (the "MPL")
-	 * http://www.mozilla.org/MPL/MPL-1.1.html
-	 *  == END LICENSE ==
-	 * 
-	 */
+/*
+ * Liferay Video plugin for CKEditor.
+ * Based on Video plugin for CKEditor Copyright (C) 2011 Alfonso Martínez de
+ * Lizarrondo
+ *  == BEGIN LICENSE ==
+ *
+ * Licensed under the terms of any of the following licenses at your choice:
+ *  - GNU General Public License Version 2 or later (the "GPL")
+ * http://www.gnu.org/licenses/gpl.html
+ *  - GNU Lesser General Public License Version 2.1 or later (the "LGPL")
+ * http://www.gnu.org/licenses/lgpl.html
+ *  - Mozilla Public License Version 1.1 or later (the "MPL")
+ * http://www.mozilla.org/MPL/MPL-1.1.html
+ *  == END LICENSE ==
+ *
+ */
 
-( function() {
+(function() {
 
-CKEDITOR.plugins.add( 'liferayvideo',{
-	
-	lang : [ 'en', 'es' ],
+CKEDITOR.plugins.add('liferayvideo',
+		{
 
-	getPlaceholderCss : function()
-	{
+	afterInit: function(editor) {
+		var dataProcessor = editor.dataProcessor;
+		var htmlFilter = dataProcessor && dataProcessor.htmlFilter;
+		var	dataFilter = dataProcessor && dataProcessor.dataFilter;
+
+		dataFilter.addRules(
+			{
+			elements : {
+					'div' : function(realElement) {
+								var attributes = realElement.attributes;
+
+								if (attributes['class'] && attributes['class'].indexOf('liferayckevideo') >= 0) {
+									var fakeElement = editor.createFakeParserElement(realElement, 'liferay_cke_video', 'liferayvideo', false);
+									var fakeStyle = fakeElement.attributes.style || '';
+
+									var width = attributes['data-width'];
+									var height = attributes['data-height'];
+									var poster = attributes['data-poster'];
+
+									if (typeof width != 'undefined') {
+										fakeElement.attributes.style = fakeStyle + 'width:' + CKEDITOR.tools.cssLength(width) + ';';
+										fakeStyle = fakeElement.attributes.style;
+									}
+
+									if (typeof height != 'undefined') {
+										fakeElement.attributes.style = fakeStyle + 'height:' + CKEDITOR.tools.cssLength(height) + ';';
+										fakeStyle = fakeElement.attributes.style;
+									}
+
+									if (poster) {
+										fakeElement.attributes.style = fakeStyle + 'background-image:url(' + poster + ');';
+										fakeStyle = fakeElement.attributes.style;
+									}
+
+									return fakeElement;
+								}
+						}
+				}
+			}
+		);
+	},
+
+	getPlaceholderCss : function() {
+		var instance = this;
+
 		return 'img.cke_video' +
 				'{' +
-					'background-image: url(' + CKEDITOR.getUrl( this.path + 'icons/placeholder.png' ) + ');' +
+					'background-image: url(' + CKEDITOR.getUrl(instance.path + 'icons/placeholder.png' ) + ');' +
 					'background-position: center center;' +
 					'background-repeat: no-repeat;' +
 					'background-color:gray;'+
@@ -35,103 +77,68 @@ CKEDITOR.plugins.add( 'liferayvideo',{
 				'}';
 	},
 
-	onLoad : function()
-	{
-		// v4
-		if (CKEDITOR.addCss)
-			CKEDITOR.addCss( this.getPlaceholderCss() );
+	init : function(editor) {
+		var instance = this;
 
-	},
+		var lang = editor.lang.liferayvideo;
 
-	init : function( editor )
-	{
-		var lang = editor.lang.liferayvideo;		
+		CKEDITOR.dialog.add('liferayvideo', instance.path + 'dialogs/video.js');
 
-		CKEDITOR.dialog.add( 'liferayvideo', this.path + 'dialogs/video.js' );
-		
-		editor.addCommand( 'LiferayVideo', new CKEDITOR.dialogCommand( 'liferayvideo'));
+		editor.addCommand('LiferayVideo', new CKEDITOR.dialogCommand('liferayvideo'));
 
-		editor.ui.addButton( 'LiferayVideo',
+		editor.ui.addButton('LiferayVideo',
 			{
-				label : lang.toolbar,
 				command : 'LiferayVideo',
-				icon : this.path + 'icons/icon.png'
-			} );		
+				icon : instance.path + 'icons/icon.png',
+				label : lang.toolbar
+			}
+		);
 
-		if ( editor.addMenuItems )
-		{
+		if (editor.addMenuItems) {
 			editor.addMenuItems(
 				{
 					liferayvideo :
 					{
-						label : lang.properties,
 						command : 'LiferayVideo',
-						group : 'flash'
+						group : 'flash',
+						label : lang.properties
 					}
 				});
 		}
 
-		editor.on( 'doubleclick', function( evt )
-			{
-				var element = evt.data.element;
+		editor.on(
+				'doubleclick', 
+				function(evt) {
+					var element = evt.data.element;
 
-				if ( element.is( 'img' ) && element.data( 'cke-real-element-type' ) == 'liferayvideo' )
-					evt.data.dialog = 'liferayvideo';
-			});
-		
-		if ( editor.contextMenu )
-		{
-			editor.contextMenu.addListener( function( element, selection )
-				{
-					if ( element && element.is( 'img' ) && !element.isReadOnly()
-							&& element.data( 'cke-real-element-type' ) == 'liferayvideo' )
-						return { video : CKEDITOR.TRISTATE_OFF };
-				});
+					if ( element.is('img') && element.data('cke-real-element-type') === 'liferayvideo') {
+						evt.data.dialog = 'liferayvideo';
+					}
+				}
+		);
+
+		if (editor.contextMenu) {
+			editor.contextMenu.addListener(
+					function(element, selection) {
+						if (element && element.is('img') && !element.isReadOnly()
+							&& element.data('cke-real-element-type') === 'liferayvideo') {
+							return { video : CKEDITOR.TRISTATE_OFF };
+						}
+					}
+			);
 		}
 		
 		editor.lang.fakeobjects.liferayvideo = lang.fakeObject;
+	},
 
+	lang : ['en', 'es'],
 
-	}, 
+	onLoad : function() {
+		var instance = this;
 
-	afterInit: function( editor )
-	{
-		var dataProcessor = editor.dataProcessor,
-			htmlFilter = dataProcessor && dataProcessor.htmlFilter,
-			dataFilter = dataProcessor && dataProcessor.dataFilter;
-		
-		dataFilter.addRules(
-			{
-
-			elements : {
-					'div' : function( realElement )
-					{						
-						if (realElement.attributes["class"] && realElement.attributes["class"].indexOf( "liferayckevideo" ) >= 0)
-						{	
-							var fakeElement = editor.createFakeParserElement( realElement, 'liferay_cke_video', 'liferayvideo', false ),
-								fakeStyle = fakeElement.attributes.style || '';
-
-							var width = realElement.attributes['data-width'],
-								height = realElement.attributes['data-height'],
-								poster = realElement.attributes['data-poster'];
-
-							if ( typeof width != 'undefined' )
-								fakeStyle = fakeElement.attributes.style = fakeStyle + 'width:' + CKEDITOR.tools.cssLength( width ) + ';';
-
-							if ( typeof height != 'undefined' )
-								fakeStyle = fakeElement.attributes.style = fakeStyle + 'height:' + CKEDITOR.tools.cssLength( height ) + ';';
-
-							if ( poster )
-								fakeStyle = fakeElement.attributes.style = fakeStyle + 'background-image:url(' + poster + ');';
-
-							return fakeElement;
-						}
-					}
-				}
-
-			}
-		);
-		
+		if (CKEDITOR.addCss) {
+			CKEDITOR.addCss(instance.getPlaceholderCss());
+		}
 	}
 
 } ); 
