693070afef5fd50e1775446be00833dd1a5ac0f0
Iliyan Peychev <iliyan.peychev@liferay.com>
2013-10-14 10:47:05 +0200

    LPS-30425 - SF

--- portal-web/docroot/html/js/liferay/dockbar.js
+++ portal-web/docroot/html/js/liferay/dockbar.js
@@ -291,9 +291,11 @@ AUI.add(
 						stagingBar.all('> li').addClass(CSS_DOCKBAR_ITEM);
 					}
 
-					navAccountControls.all('> li > a').each(function() {
-						this.ancestor().addClass(CSS_DOCKBAR_ITEM);
-					});
+					navAccountControls.all('> li > a').each(
+						function(item, index, collection) {
+							item.ancestor().addClass(CSS_DOCKBAR_ITEM);
+						}
+					);
 				}
 
 				if (splitMode) {
@@ -316,9 +318,11 @@ AUI.add(
 
 						navAddControls.focusManager.after(
 							'focusedChange',
-							function (event) {
+							function(event) {
+								var instance = this;
+
 								if (!event.newVal) {
-									this.set('activeDescendant', 0);
+									instance.set('activeDescendant', 0);
 								}
 							}
 						);
@@ -327,9 +331,11 @@ AUI.add(
 				else {
 					if (navAddControls) {
 						if (brand) {
-							brand.all('a').each(function() {
-								this.ancestor().addClass(CSS_DOCKBAR_ITEM);
-							});
+							brand.all('a').each(
+								function(item, index, collection) {
+									item.ancestor().addClass(CSS_DOCKBAR_ITEM);
+								}
+							);
 						}
 
 						navAddControls.all('> li').addClass(CSS_DOCKBAR_ITEM);
@@ -342,8 +348,10 @@ AUI.add(
 					var navAccountControlsAncestor = target.ancestor(SELECTOR_NAV_ACCOUNT_CONTROLS);
 
 					if (navAccountControlsAncestor) {
-						navAccountControlsAncestor.one('li a').blur();
-						navAccountControlsAncestor.one('li a').focus();
+						var navLink = navAccountControlsAncestor.one('li a');
+
+						navLink.blur();
+						navLink.focus();
 					}
 					else {
 						target.blur();
