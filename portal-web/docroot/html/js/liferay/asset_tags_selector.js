c42d6dbad6283d9fc10a238a51f3e92bce33462c
Marc Lundgren <marc.lundgren@liferay.com>
2013-10-25 15:08:17 -0700

    LPS-41669  Tag suggestion showing wrong result

--- portal-web/docroot/html/js/liferay/asset_tags_selector.js
+++ portal-web/docroot/html/js/liferay/asset_tags_selector.js
@@ -527,8 +527,6 @@ AUI.add(
 
 						var context = STR_BLANK;
 
-						var data = [];
-
 						if (contentCallback) {
 							context = contentCallback();
 
@@ -547,16 +545,23 @@ AUI.add(
 							function(response) {
 								var results = response.query.results;
 
+								var data = [];
+
 								if (results) {
 									var resultData = results.Result;
 
-									for (var i = 0; i < resultData.length; i++) {
-										data.push(
-											{
-												name: resultData[i]
-											}
-										);
+									if (!A.Lang.isArray(resultData)) {
+										resultData = [resultData];
 									}
+
+									data = AArray.map(
+										resultData,
+										function(item, index, collection) {
+											return {
+												name: item
+											};
+										}
+									);
 								}
 
 								instance._updateSelectList(AArray.unique(data));
