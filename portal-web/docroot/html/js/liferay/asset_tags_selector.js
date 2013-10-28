19ef77105711f98f657b20d7866f29db4d649be7
Jonathan Mak <jonathan.mak@liferay.com>
2013-10-28 16:28:35 -0700

    LPS-41669 - Source formatting

--- portal-web/docroot/html/js/liferay/asset_tags_selector.js
+++ portal-web/docroot/html/js/liferay/asset_tags_selector.js
@@ -247,7 +247,7 @@ AUI.add(
 
 							bodyNode.html(STR_BLANK);
 
-							var searchForm = A.Node.create(A.Lang.sub(TPL_SEARCH_FORM, [Liferay.Language.get('search')]));
+							var searchForm = A.Node.create(Lang.sub(TPL_SEARCH_FORM, [Liferay.Language.get('search')]));
 
 							bodyNode.append(searchForm);
 
@@ -550,7 +550,7 @@ AUI.add(
 								if (results) {
 									var resultData = results.Result;
 
-									if (!A.Lang.isArray(resultData)) {
+									if (!Lang.isArray(resultData)) {
 										resultData = [resultData];
 									}
 
