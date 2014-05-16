da07cabebfc0f6a95a2aea55749a91e494c21850
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2014-05-16 10:40:19 +0200

    LPS-46224 Adds new parameter modified date to diff taglib

--- portal-impl/src/com/liferay/portlet/wiki/util/WikiUtil.java
+++ portal-impl/src/com/liferay/portlet/wiki/util/WikiUtil.java
@@ -236,8 +236,8 @@ public class WikiUtil {
 			}
 
 			DiffVersion diffVersion = new DiffVersion(
-				page.getUserId(), page.getVersion(), page.getSummary(),
-				extraInfo);
+				page.getUserId(), page.getVersion(), page.getModifiedDate(),
+				page.getSummary(), extraInfo);
 
 			diffVersions.add(diffVersion);
 		}
