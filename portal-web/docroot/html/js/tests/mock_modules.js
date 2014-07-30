e6dc7bfc4649e9a45d90a38243c7f4de63dedbe9
Bruno Basto <bruno.basto@liferay.com>
2014-07-30 15:12:56 -0300

    Adding barebone files. This is just what I needed so far. We will definetely need to add some other files. I'm not sure mapping them to mock_modules.js is the way to go. We may just add them directly in the index.html of each test.

--- portal-web/docroot/html/js/tests/mock_modules.js
+++ portal-web/docroot/html/js/tests/mock_modules.js
@@ -23,3 +23,52 @@ YUI_config.groups.mock = {
 	},
 	root: Liferay.AUI.getJavaScriptRootPath() + '/liferay/'
 };
+
+YUI_config.groups.utility = {
+	base: Liferay.AUI.getJavaScriptRootPath() + '/liferay/',
+	modules: {
+		'liferay-utility-dependency': {
+			condition: {
+				name: 'liferay-utility-dependency',
+				trigger: 'test'
+			},
+			path: 'dependency.js',
+			requires: ['oop']
+		},
+		'liferay-utility-events': {
+			condition: {
+				name: 'liferay-utility-events',
+				trigger: 'test'
+			},
+			path: 'events.js',
+			requires: ['liferay-utility-dependency']
+		},
+		'liferay-utility-language': {
+			condition: {
+				name: 'liferay-utility-language',
+				trigger: 'test'
+			},
+			path: 'language.js'
+		},
+		'liferay-utility-liferay': {
+			condition: {
+				name: 'liferay-utility-liferay',
+				trigger: 'test'
+			},
+			path: 'liferay.js',
+			requires: ['oop']
+		},
+		'liferay-utility-util': {
+			condition: {
+				name: 'liferay-utility-util',
+				trigger: 'test'
+			},
+			path: 'util.js',
+			requires: [
+				'liferay-utility-events',
+				'liferay-utility-liferay'
+			]
+		}
+	},
+	root: Liferay.AUI.getJavaScriptRootPath() + '/liferay/'
+};
