abb92760b61dce6479c86c307300c9ba3232e9a3
Bruno Basto <bruno.basto@liferay.com>
2014-07-31 11:59:30 -0300

    SF

--- portal-web/docroot/html/js/liferay/tests/test_util.js
+++ portal-web/docroot/html/js/liferay/tests/test_util.js
@@ -1,31 +1,40 @@
-AUI.add('liferay-util-tests', function(A) {
-	var suite = new A.Test.Suite('liferay-util');
+AUI.add(
+	'liferay-util-tests',
+	function(A) {
+		var suite = new A.Test.Suite('liferay-util');
 
-	suite.add(new A.Test.Case({
-		name: 'Liferay.Util tests',
+		suite.add(
+			new A.Test.Case(
+				{
+					name: 'Liferay.Util tests',
 
-		setUp: function () {
-			//
-		},
+					setUp: function() {
+						//
+					},
 
-		tearDown: function () {
-			//
-		},
+					tearDown: function() {
+						//
+					},
 
-		'Test Liferay.Util.camelize': function() {
-			A.Assert.areSame(
-				Liferay.Util.camelize('this-will-pass'),
-				'thisWillPass'
-			);
+					'Test Liferay.Util.camelize': function() {
+						A.Assert.areSame(
+							Liferay.Util.camelize('this-will-pass'),
+							'thisWillPass'
+						);
 
-			A.Assert.areNotSame(
-				Liferay.Util.camelize('this-will-fail'),
-				'thisWillfail'
-			);
-		}
-	}));
+						A.Assert.areNotSame(
+							Liferay.Util.camelize('this-will-fail'),
+							'thisWillfail'
+						);
+					}
+				}
+			)
+		);
 
-	A.Test.Runner.add(suite);
-}, '', {
-	requires: ['test']
-});
+		A.Test.Runner.add(suite);
+	},
+	'',
+	{
+		requires: ['test']
+	}
+);
--- portal-web/docroot/html/portlet/dynamic_data_mapping/js/tests/test_dynamic_data_mapping.js
+++ portal-web/docroot/html/portlet/dynamic_data_mapping/js/tests/test_dynamic_data_mapping.js
@@ -1,179 +1,188 @@
-AUI.add('liferay-portlet-dynamic-data-mapping-tests', function(A) {
-	var AArray = A.Array;
+AUI.add(
+	'liferay-portlet-dynamic-data-mapping-tests',
+	function(A) {
+		var AArray = A.Array;
 
-	var suite = new A.Test.Suite('liferay-portlet-dynamic-data-mapping');
+		var suite = new A.Test.Suite('liferay-portlet-dynamic-data-mapping');
 
-	var getTestDefinition = function(name, callback) {
-		A.io(
-			name,
-			{
-				on: {
-					complete: callback
-				},
-				type: 'GET'
-			}
-		);
-	};
-
-	var liferayFormBuilder;
-
-	var assertLocalizationMapIsCorrect = function(originalField, deserializedField, localizableAttributes) {
-		var localizationMap = deserializedField.localizationMap;
-
-		A.Assert.isNotUndefined(
-			localizationMap,
-			'Every field should have a localizationMap.'
-		);
-
-		AArray.each(
-			localizableAttributes,
-			function(attribute) {
-				if (attribute !== 'options') {
-					A.each(
-						originalField[attribute],
-						function(originalValue, originalLanguageId) {
-							A.Assert.isNotUndefined(
-								localizationMap[originalLanguageId],
-								'Translation to [' + originalLanguageId + '] should be present in localizationMap.'
-							);
-
-							A.Assert.areSame(
-								localizationMap[originalLanguageId][attribute],
-								originalValue,
-								'Value in [' + originalLanguageId + '] for attribute [' + attribute + '] should be the same as it was before deserialization.'
-							);
-						}
-					);
-				}
-				else {
-					AArray.each(
-						originalField[attribute],
-						function(originalFieldOptionsItem, index) {
-							var deserializedFieldOptions = deserializedField[attribute];
-
-							assertLocalizationMapIsCorrect(
-								originalFieldOptionsItem,
-								deserializedFieldOptions[index],
-								['label']
-							);
-						}
-					)
-				}
-			}
-		);
-
-		var originalFieldNestedFields = originalField.nestedFields;
-
-		var deserializedFieldNestedFields = deserializedField.fields;
-
-		if (A.Lang.isArray(originalFieldNestedFields)) {
-			AArray.each(
-				originalFieldNestedFields,
-				function(originalChildField, index) {
-					assertLocalizationMapIsCorrect(
-						originalChildField,
-						deserializedFieldNestedFields[index],
-						localizableAttributes
-					);
+		var getTestDefinition = function(name, callback) {
+			A.io(
+				name,
+				{
+					on: {
+						complete: callback
+					},
+					type: 'GET'
 				}
 			);
-		}
-	};
+		};
 
-	var assertRenamedNestedFields = function(originalField, deserializedField) {
-		var originalFieldNestedFields = originalField.nestedFields;
+		var liferayFormBuilder;
 
-		var deserializedFieldNestedFields = deserializedField.fields;
+		var assertLocalizationMapIsCorrect = function(originalField, deserializedField, localizableAttributes) {
+			var localizationMap = deserializedField.localizationMap;
 
-		if (originalField.nestedFields) {
-			A.Assert.isUndefined(deserializedField.nestedFields, 'Deserialization should remove attribute [nestedFields] from fields.');
+			A.Assert.isNotUndefined(
+				localizationMap,
+				'Every field should have a localizationMap.'
+			);
 
-			A.Assert.isNotUndefined(deserializedField.fields, 'Deserialization should rename attribute [nestedFields] to [fields].');
-		}
-	};
+			AArray.each(
+				localizableAttributes,
+				function(attribute) {
+					if (attribute !== 'options') {
+						A.each(
+							originalField[attribute],
+							function(originalValue, originalLanguageId) {
+								A.Assert.isNotUndefined(
+									localizationMap[originalLanguageId],
+									'Translation to [' + originalLanguageId + '] should be present in localizationMap.'
+								);
 
-	var assertUnlocalizableAttributesValuesAreSame = function(originalField, deserializedField) {
-		AArray.each(
-			Liferay.FormBuilder.UNLOCALIZABLE_FIELD_ATTRS,
-			function(attribute) {
-				A.Assert.areSame(
-					originalField[attribute],
-					deserializedField[attribute],
-					'Unlocalizable attribute [' + attribute + '] should be the same before and after deserialization.'
+								A.Assert.areSame(
+									localizationMap[originalLanguageId][attribute],
+									originalValue,
+									'Value in [' + originalLanguageId + '] for attribute [' + attribute + '] should be the same as it was before deserialization.'
+								);
+							}
+						);
+					}
+					else {
+						AArray.each(
+							originalField[attribute],
+							function(originalFieldOptionsItem, index) {
+								var deserializedFieldOptions = deserializedField[attribute];
+
+								assertLocalizationMapIsCorrect(
+									originalFieldOptionsItem,
+									deserializedFieldOptions[index],
+									['label']
+								);
+							}
+						)
+					}
+				}
+			);
+
+			var originalFieldNestedFields = originalField.nestedFields;
+
+			var deserializedFieldNestedFields = deserializedField.fields;
+
+			if (A.Lang.isArray(originalFieldNestedFields)) {
+				AArray.each(
+					originalFieldNestedFields,
+					function(originalChildField, index) {
+						assertLocalizationMapIsCorrect(
+							originalChildField,
+							deserializedFieldNestedFields[index],
+							localizableAttributes
+						);
+					}
 				);
 			}
-		);
-	};
+		};
 
-	suite.add(new A.Test.Case({
-		name: 'Dynamic Data Mapping tests',
+		var assertRenamedNestedFields = function(originalField, deserializedField) {
+			var originalFieldNestedFields = originalField.nestedFields;
 
-		setUp: function () {
-			liferayFormBuilder = new Liferay.FormBuilder();
-		},
+			var deserializedFieldNestedFields = deserializedField.fields;
 
-		tearDown: function () {
-			liferayFormBuilder.destroy();
-		},
+			if (originalField.nestedFields) {
+				A.Assert.isUndefined(deserializedField.nestedFields, 'Deserialization should remove attribute [nestedFields] from fields.');
 
-		'Test DDM Definition Field Names': function() {
-			var validateStructureFieldName = A.config.FormValidator.RULES.structureFieldName;
+				A.Assert.isNotUndefined(deserializedField.fields, 'Deserialization should rename attribute [nestedFields] to [fields].');
+			}
+		};
 
-			A.Assert.isFalse(
-				validateStructureFieldName('inva.lid,name'),
-				'Do not allow punctuation.'
-			);
-
-			A.Assert.isFalse(
-				validateStructureFieldName('-invalidname'),
-				'Cannot start with hifen.'
-			);
-
-			A.Assert.isTrue(
-				validateStructureFieldName('validname'),
-				'Allow alphanumeric characters.'
-			);
-		},
-
-		'Test DDM Fields Deserialization': function() {
-			var test = this;
-
-			getTestDefinition(
-				'ddm-definition.json',
-				function(i, o) {
-					var original = JSON.parse(o.responseText);
-					var deserializable = JSON.parse(o.responseText);
-
-					test.resume(
-						function() {
-							var deserializedFields = liferayFormBuilder.deserializeDefinitionFields(deserializable);
-
-							AArray.each(
-								deserializedFields,
-								function(deserializedField, index) {
-									var originalField = original.fields[index];
-
-									assertRenamedNestedFields(originalField, deserializedField);
-
-									assertUnlocalizableAttributesValuesAreSame(originalField, deserializedField);
-
-									assertLocalizationMapIsCorrect(
-										originalField,
-										deserializedField,
-										Liferay.FormBuilder.LOCALIZABLE_FIELD_ATTRS
-									);
-								}
-							);
-						}
+		var assertUnlocalizableAttributesValuesAreSame = function(originalField, deserializedField) {
+			AArray.each(
+				Liferay.FormBuilder.UNLOCALIZABLE_FIELD_ATTRS,
+				function(attribute) {
+					A.Assert.areSame(
+						originalField[attribute],
+						deserializedField[attribute],
+						'Unlocalizable attribute [' + attribute + '] should be the same before and after deserialization.'
 					);
 				}
 			);
+		};
 
-			test.wait();
-		}
-	}));
+		suite.add(
+			new A.Test.Case(
+				{
+					name: 'Dynamic Data Mapping tests',
 
-	A.Test.Runner.add(suite);
-}, '', {
-	requires: ['io-base', 'test', 'liferay-portlet-dynamic-data-mapping-custom-fields']
-});
+					setUp: function() {
+						liferayFormBuilder = new Liferay.FormBuilder();
+					},
+
+					tearDown: function() {
+						liferayFormBuilder.destroy();
+					},
+
+					'Test DDM Definition Field Names': function() {
+						var validateStructureFieldName = A.config.FormValidator.RULES.structureFieldName;
+
+						A.Assert.isFalse(
+							validateStructureFieldName('inva.lid,name'),
+							'Do not allow punctuation.'
+						);
+
+						A.Assert.isFalse(
+							validateStructureFieldName('-invalidname'),
+							'Cannot start with hifen.'
+						);
+
+						A.Assert.isTrue(
+							validateStructureFieldName('validname'),
+							'Allow alphanumeric characters.'
+						);
+					},
+
+					'Test DDM Fields Deserialization': function() {
+						var test = this;
+
+						getTestDefinition(
+							'ddm-definition.json',
+							function(i, o) {
+								var original = JSON.parse(o.responseText);
+								var deserializable = JSON.parse(o.responseText);
+
+								test.resume(
+									function() {
+										var deserializedFields = liferayFormBuilder.deserializeDefinitionFields(deserializable);
+
+										AArray.each(
+											deserializedFields,
+											function(deserializedField, index) {
+												var originalField = original.fields[index];
+
+												assertRenamedNestedFields(originalField, deserializedField);
+
+												assertUnlocalizableAttributesValuesAreSame(originalField, deserializedField);
+
+												assertLocalizationMapIsCorrect(
+													originalField,
+													deserializedField,
+													Liferay.FormBuilder.LOCALIZABLE_FIELD_ATTRS
+												);
+											}
+										);
+									}
+								);
+							}
+						);
+
+						test.wait();
+					}
+				}
+			)
+		);
+
+		A.Test.Runner.add(suite);
+	},
+	'',
+	{
+		requires: ['io-base', 'test', 'liferay-portlet-dynamic-data-mapping-custom-fields']
+	}
+);
