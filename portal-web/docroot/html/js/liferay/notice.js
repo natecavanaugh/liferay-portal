200e5055fbd1fab0fe38bb4848f79f00ec3d9126
Jonathan Mak <jonathan.mak@liferay.com>
2014-09-10 14:37:03 -0700

    LPS-49726 - General source formatting

--- portal-web/docroot/html/js/liferay/notice.js
+++ portal-web/docroot/html/js/liferay/notice.js
@@ -149,7 +149,7 @@ AUI.add(
 				}
 
 				if (instance._useCloseButton) {
-					var html =  '<button class="btn btn-default submit popup-alert-close">' +
+					var html = '<button class="btn btn-default submit popup-alert-close">' +
 									instance._closeText +
 								'</button>';
 
