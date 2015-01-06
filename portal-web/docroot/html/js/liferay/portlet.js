cb25b0b744b04c330127bfab9f88f12dc06ea9bb
Dustin Ryerson <dustin.ryerson@liferay.com>
2015-01-06 11:38:54 -0800

    LPS-52461 Don't show UI error if Ajax request is interrupted

--- portal-web/docroot/html/js/liferay/portlet.js
+++ portal-web/docroot/html/js/liferay/portlet.js
@@ -342,9 +342,11 @@
 					dataType: dataType,
 					on: {
 						failure: function(event, id, obj) {
-							placeHolder.hide();
+							if ((obj.readyState != 4) && (obj.status != 0)) {
+								placeHolder.hide();
 
-							placeHolder.placeAfter('<div class="alert alert-danger">' + Liferay.Language.get('there-was-an-unexpected-error.-please-refresh-the-current-page') + '</div>');
+								placeHolder.placeAfter('<div class="alert alert-danger">' + Liferay.Language.get('there-was-an-unexpected-error.-please-refresh-the-current-page') + '</div>');
+							}
 						},
 						success: function(event, id, obj) {
 							var instance = this;
