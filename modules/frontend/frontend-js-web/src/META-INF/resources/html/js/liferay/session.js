f4bfd6254107894f976665fbf521db9fa8a55176
Jonathan Mak <jonathan.mak@liferay.com>
2015-05-26 17:25:51 -0700

    LPS-55665 - General source formatting

--- modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/session.js
+++ modules/frontend/frontend-js-web/src/META-INF/resources/html/js/liferay/session.js
@@ -130,12 +130,13 @@ AUI.add(
 					_afterSessionStateChange: function(event) {
 						var instance = this;
 
+						var details = event.details;
 						var newVal = event.newVal;
 
 						var src = null;
 
-						if (('src' in event) && event.details.length) {
-							src = event.details[0];
+						if ('src' in event && details.length) {
+							src = details[0];
 						}
 
 						instance.fire(MAP_SESSION_STATE_EVENTS[newVal] || newVal, src);
@@ -256,7 +257,7 @@ AUI.add(
 						A.on(
 							'io:complete',
 							function(transactionId, response, args) {
-								if (!args || (args && (args.sessionExtend || !Lang.isBoolean(args.sessionExtend)))) {
+								if (!args || args && args.sessionExtend || !Lang.isBoolean(args.sessionExtend)) {
 									instance.resetInterval();
 								}
 							}
@@ -294,25 +295,26 @@ AUI.add(
 					_startTimer: function() {
 						var instance = this;
 
-						var warningTime = instance.get('warningTime');
 						var sessionLength = instance.get('sessionLength');
+						var warningTime = instance.get('warningTime');
 
 						instance._elapsed = 0;
 
 						var registered = instance._registered;
+
 						var interval = 1000;
 
 						instance._intervalId = A.setInterval(
 							function() {
-								var elapsed = (instance._elapsed += 1000);
+								var elapsed = instance._elapsed += 1000;
 
 								var extend = false;
 
-								var expirationMoment = (elapsed == sessionLength);
-								var warningMoment = (elapsed == warningTime);
+								var expirationMoment = elapsed == sessionLength;
+								var warningMoment = elapsed == warningTime;
 
-								var hasExpired = (elapsed >= sessionLength);
-								var hasWarned = (elapsed >= warningTime);
+								var hasExpired = elapsed >= sessionLength;
+								var hasWarned = elapsed >= warningTime;
 
 								var updateSessionState = true;
 
