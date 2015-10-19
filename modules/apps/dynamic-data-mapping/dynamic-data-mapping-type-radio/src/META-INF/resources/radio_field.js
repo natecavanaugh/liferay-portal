d15da061d8afc88980fa23037009f55131f5c206
Bruno Basto <bruno.basto@liferay.com>
2015-10-19 17:51:47 -0300

    LPS-59604 Setting and getting values correctly so that visibility computation is accurate

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio_field.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-type-radio/src/META-INF/resources/radio_field.js
@@ -1,6 +1,8 @@
 AUI.add(
 	'liferay-ddm-form-field-radio',
 	function(A) {
+		var Lang = A.Lang;
+
 		var RadioField = A.Component.create(
 			{
 				ATTRS: {
@@ -28,6 +30,28 @@ AUI.add(
 							value = value[instance.get('locale')];
 						}
 
+						var predefinedValue = instance.get('predefinedValue');
+
+						if (!value && predefinedValue) {
+							if (Lang.isObject(predefinedValue)) {
+								value = predefinedValue[instance.get('locale')];
+							}
+							else {
+								value = predefinedValue;
+							}
+						}
+
+						if (!Lang.isArray(value)) {
+							try {
+								value = JSON.parse(value);
+							}
+							catch (e) {
+								value = [value];
+							}
+						}
+
+						value = value[0];
+
 						return A.map(
 							instance.get('options'),
 							function(item) {
@@ -49,6 +73,34 @@ AUI.add(
 								options: instance.getOptions()
 							}
 						);
+					},
+
+					setValue: function(value) {
+						var instance = this;
+
+						var container = instance.get('container');
+
+						var radiosNodeList = container.all(instance.getInputSelector());
+
+						radiosNodeList.attr('checked', false);
+
+						var radiosToCheck = radiosNodeList.filter(
+							function(node) {
+								return node.val() === value;
+							}
+						);
+
+						radiosToCheck.attr('checked', true);
+					},
+
+					_renderErrorMessage: function() {
+						var instance = this;
+
+						var container = instance.get('container');
+
+						RadioField.superclass._renderErrorMessage.apply(instance, arguments);
+
+						container.all('.validation-message').appendTo(container);
 					}
 				}
 			}
