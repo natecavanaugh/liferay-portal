557fc58ce405c2d2724f23c60e45070a4387a582
Jonathan Mak <jonathan.mak@liferay.com>
2015-11-12 11:32:13 -0800

    LPS-59844 - Move event handle binding to bindUI.

--- modules/apps/server-admin/server-admin-web/src/main/resources/META-INF/resources/js/main.js
+++ modules/apps/server-admin/server-admin-web/src/main/resources/META-INF/resources/js/main.js
@@ -39,23 +39,27 @@ AUI.add(
 					initializer: function() {
 						var instance = this;
 
-						var eventHandles = [];
+						instance._eventHandles = [];
+					},
 
-						eventHandles.push(
+					bindUI: function() {
+						var instance = this;
+
+						instance._eventHandles.push(
 							instance.get('form').delegate(
 								STR_CLICK,
 								A.bind('_onSubmit', instance),
 								instance.get('submitButton')
 							)
 						);
-
-						instance._eventHandles = eventHandles;
 					},
 
 					destructor: function() {
 						var instance = this;
 
 						A.Array.invoke(instance._eventHandles, 'detach');
+
+						instance._eventHandles = null;
 					},
 
 					_addInputsFromData: function(data) {
