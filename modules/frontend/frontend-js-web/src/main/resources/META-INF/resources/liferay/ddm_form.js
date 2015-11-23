a747164d4d50885913180cfc6017a191abb51a4c
Bruno Basto <bruno.basto@liferay.com>
2015-11-23 19:15:54 -0300

    LPS-55710 Tooltips are not displaying for DDM fields

--- modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/ddm_form.js
+++ modules/frontend/frontend-js-web/src/main/resources/META-INF/resources/liferay/ddm_form.js
@@ -117,6 +117,8 @@ AUI.add(
 			_getField: function(fieldNode) {
 				var instance = this;
 
+				var displayLocale = instance.get('displayLocale');
+
 				var fieldInstanceId = instance.extractInstanceId(fieldNode);
 
 				var fieldName = fieldNode.getData('fieldName');
@@ -134,7 +136,7 @@ AUI.add(
 							container: fieldNode,
 							dataType: fieldDefinition.dataType,
 							definition: definition,
-							displayLocale: instance.get('displayLocale'),
+							displayLocale: displayLocale,
 							instanceId: fieldInstanceId,
 							name: fieldName,
 							parent: instance,
@@ -316,6 +318,20 @@ AUI.add(
 						return field;
 					},
 
+					getDefaulLocale: function() {
+						var instance = this;
+
+						var definition = instance.get('definition');
+
+						var defaultLocale = themeDisplay.getDefaultLanguageId();
+
+						if (definition) {
+							defaultLocale = definition.defaultLanguageId;
+						}
+
+						return defaultLocale;
+					},
+
 					getFieldDefinition: function() {
 						var instance = this;
 
@@ -442,8 +458,26 @@ AUI.add(
 
 						var labelNode = instance.getLabelNode();
 
+						var tipNode = labelNode.one('.taglib-icon-help');
+
 						if (Lang.isValue(label) && Lang.isNode(labelNode)) {
 							labelNode.html(A.Escape.html(label));
+						}
+
+						if (tipNode) {
+							var defaultLocale = instance.getDefaulLocale();
+
+							var fieldDefinition = instance.getFieldDefinition();
+
+							var tipsMap = fieldDefinition.tip;
+
+							if (Lang.isObject(tipsMap)) {
+								var tip = tipsMap[instance.get('displayLocale')] || tipsMap[defaultLocale];
+
+								tipNode.one('.tooltip-text').html(A.Escape.html(tip));
+							}
+
+							labelNode.append(tipNode);
 						}
 					},
 
@@ -460,17 +494,11 @@ AUI.add(
 					syncLabelUI: function() {
 						var instance = this;
 
+						var defaultLocale = instance.getDefaulLocale();
+
 						var fieldDefinition = instance.getFieldDefinition();
 
 						var labelsMap = fieldDefinition.label;
-
-						var definition = instance.get('definition');
-
-						var defaultLocale = themeDisplay.getDefaultLanguageId();
-
-						if (definition) {
-							defaultLocale = definition.defaultLanguageId;
-						}
 
 						var label = labelsMap[instance.get('displayLocale')] || labelsMap[defaultLocale];
 
