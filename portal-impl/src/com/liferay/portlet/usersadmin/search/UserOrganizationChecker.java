23e9268f75fd1ad1a895895fc85347712cd2f4eb
Pei-Jung Lan <pei-jung.lan@liferay.com>
2016-11-03 15:24:22 -0700

    LPS-66309 Users already assigned to organization should be disabled in the selection list

--- portal-impl/src/com/liferay/portlet/usersadmin/search/UserOrganizationChecker.java
+++ portal-impl/src/com/liferay/portlet/usersadmin/search/UserOrganizationChecker.java
@@ -14,7 +14,7 @@
 
 package com.liferay.portlet.usersadmin.search;
 
-import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Organization;
@@ -33,7 +33,7 @@ import javax.portlet.RenderResponse;
  * @author Brian Wing Shun Chan
  * @author Jorge Ferrer
  */
-public class UserOrganizationChecker extends RowChecker {
+public class UserOrganizationChecker extends EmptyOnClickRowChecker {
 
 	public UserOrganizationChecker(
 		RenderResponse renderResponse, Organization organization) {
@@ -60,6 +60,10 @@ public class UserOrganizationChecker extends RowChecker {
 
 	@Override
 	public boolean isDisabled(Object obj) {
+		if (isChecked(obj)) {
+			return true;
+		}
+
 		if (!PropsValues.ORGANIZATIONS_ASSIGNMENT_STRICT) {
 			return false;
 		}
